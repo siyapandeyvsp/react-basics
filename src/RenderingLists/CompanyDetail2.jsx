d7bdee89c332df98970d51bd3e303cb2b390c6a0
siyapandeyvsp <siyapandeyvsp@gmail.com>
2024-04-19 18:35:15 +0530

    use spread and 3 object functions for rendering company data

--- src/RenderingLists/CompanyDetail2.jsx
+++ src/RenderingLists/CompanyDetail2.jsx
@@ -4,18 +4,15 @@
 // const CompanyDetail2 = () => {
 //   return (
 //     <div className='m-5 p-5 flex flex-col items-center justify-center'>
-//       {/* Map through each company */}
+
 //       {companyData2.map((company, index) => (
 //         <div key={index}>
-//           {/* Display company name */}
 
-//           {/* Map through employees of the current company */}
 //           {Object.entries(company).map(([companyKey, employees], empIndex) => (
 //             <div key={empIndex}>
-//               {/* Display company key */}
+
 //               <h2 className='text-lg font-bold text-blue-500'>Company : {companyKey}.pvt.ltd</h2>
 
-//               {/* Render employee table */}
 //               <table border='1' className='m-2 border-4'>
 //                 <thead>
 //                   <tr >
@@ -25,7 +22,7 @@
 //                   </tr>
 //                 </thead>
 //                 <tbody>
-//                   {/* Map through employees */}
+
 //                   {employees.map((employee, empId) => (
 //                     <tr  key={empId}>
 //                       <td className='border-4'>{employee.id}</td>
@@ -36,7 +33,7 @@
 //                 </tbody>
 //               </table>
 //             </div>
-//           ))}       
+//           ))}
 //         </div>
 //       ))}
 //     </div>
@@ -44,19 +41,30 @@
 // };
 
 // export default CompanyDetail2;
-import React from 'react'
-import { companyData2 } from '../data'
-const CompanyDetail2 = () => {
-    console.log("company data ",companyData2)
-    const data=Object.entries(companyData2[0])
-    console.log("inside ",data)
-   Object.values(data)
+
+import React from "react";
+import { companyData2 } from "../data";
+
+const CompanyDetail2 = () => { 
+  const companies = Object.entries(companyData2[0]);
   return (
     <div>
-      hi
-      {/* {data.map(cid=><div>Company Name : {Object.values(data).map(cname=><p>{cname}</p>)}</div>)} */}
+      <h1 className="font-bold text-2xl text-center text-blue-900"> Company Details </h1>
+      {companies.map((company) => (
+        <div key={company}>
+          <h1 className="text-blue-500  text-xl font-bold">Company : {company[0]}.pvt.ltd </h1>
+          <table>
+            <thead>
+              <tr> {Object.keys(...company[1]).map(head=><th key={head}className="border-4 p-5">{head}</th>)}</tr>
+            </thead>
+            <tbody>
+            {company[1].map((employee,i)=><tr key={i}>{Object.values(employee).map((data,i)=>(<td className="border-4 p-5" key={i}>{data}</td>))}</tr>)}
+            </tbody>
+          </table>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default CompanyDetail2
+export default CompanyDetail2;
